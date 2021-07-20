4545a8ba824b797e8286d52843e1c85e53c81e82
Marco9419 <84870105+Marco9419@users.noreply.github.com>
2021-07-20 10:16:51 -0500

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,23 @@
+
+public class main {
+
+		public static void main(String[] args) {
+			
+			byte numCorto = 122;
+			byte otroNum = 122;
+			int suma = numCorto + otroNum;
+			
+			long num1 = 1000;
+			int num2 = (int) num1;
+			float num3 = num2;
+			
+			System.out.println(num1);
+			System.out.println(num2);
+			System.out.println(num3);
+			System.out.println(suma);
+		}
+
+	}
+
+	
+
